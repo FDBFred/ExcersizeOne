1e7fcb9bad650d0f7164bddc5a238bd85847b885
Freddie Broxis <freddiebroxis@gmail.com>
2023-08-14 11:31:55 +0200

    Initial commit

---
+++ Main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+var score float64 = 0
+
+func main() {
+
+	file, err := os.Open("quiz-master/problems.csv")
+	if err != nil {
+		log.Fatalln("Error opening file", err)
+	}
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			return
+		}
+	}(file)
+
+	// Read the file
+	reader := csv.NewReader(file)
+
+	reader.Comma = ','
+
+	records, err := reader.ReadAll()
+
+	fmt.Println("Welcome to the Quiz Game")
+	fmt.Println("Please answer the following questions")
+
+	time.Sleep(5 * time.Second)
+
+	startTime := time.Now()
+
+	for i := 0; i < len(records); i++ {
+		fmt.Println(records[i][0])
+
+		var answer int
+
+		_, err := fmt.Scanf("%d\n", &answer)
+		if err != nil {
+			fmt.Println("Enter A Number")
+		}
+
+		ansCorr, err := strconv.Atoi(records[i][1])
+
+		if answer == ansCorr {
+			score += 1
+		}
+	}
+
+	endTime := time.Now()
+
+	scoreTime := endTime.Sub(startTime)
+
+	fmt.Println("Time taken: ", scoreTime)
+
+	fmt.Println("Your score was: ", score)
+
+	scoreTimeSecs := scoreTime.Seconds()
+
+	LengthAsFloat := float64(len(records))
+
+	fmt.Println("Your overall score is: ", ((LengthAsFloat-score)+1)*scoreTimeSecs)
+
+	time.Sleep(10 * time.Second)
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module awesomeProject1
+
+go 1.21
+
+require github.com/bradhe/stopwatch v0.0.0-20190618212248-a58cccc508ea
